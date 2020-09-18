668c9a715c1ac46156cff66f46e5a3a063ce7176
nerodroid <nerodroid.dev@gmail.com>
2020-09-18 06:07:09 +0530

    first commit .added the java file

---
+++ CSCExamples.java
@@ -0,0 +1,50 @@
+class CSCExamples{
+
+	int QuestionNo;
+	String category;
+	String text;
+
+
+	
+	public void setQuestionNo(int QuestionNo){
+		
+
+	}
+
+	public void setCategory(String category){
+		
+	}
+
+	public void setText(Stirng text){
+	
+	}
+
+	public int getQuestionNo(){
+		
+	}
+	
+	public String getCategory(){
+		
+		
+	}
+	public int getText(){
+		
+	}
+	public void addQuestion(CSCExamples question){
+		
+		
+	}
+	
+	public void removeQuestion(int qID){
+		
+		
+	}
+	
+	public void editQuestion(int qID){
+		
+		
+	}
+	
+
+
+}
