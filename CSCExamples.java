663cf096d886081b745c4e895c86d5e50899f17e
nerodroid <nerodroid.dev@gmail.com>
2020-09-18 06:09:15 +0530

    added getters and setters

--- CSCExamples.java
+++ CSCExamples.java
@@ -7,28 +7,28 @@ class CSCExamples{
 
 	
 	public void setQuestionNo(int QuestionNo){
-		
+		this.QuestionNo=QuestionNo;
 
 	}
 
 	public void setCategory(String category){
-		
+		this.category=category;
 	}
 
 	public void setText(Stirng text){
-	
+		this.text=text;
 	}
 
 	public int getQuestionNo(){
-		
+		return QuestionNo;
 	}
 	
 	public String getCategory(){
-		
+		return category;
 		
 	}
 	public int getText(){
-		
+		return text;
 	}
 	public void addQuestion(CSCExamples question){
 		
